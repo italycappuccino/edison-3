f7b32e1ca95e354f22fed6118d81a27098d7a9b6
Peter Liu <italycappuccino@gmail.com>
2016-12-20 16:49:14 +0800

    init
    edison3 init

---
+++ src/main/java/org/stone/Application.java
@@ -0,0 +1,14 @@
+package org.stone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Peter on 12/3/16.
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) throws Exception{
+        SpringApplication.run(Application.class,args);
+    }
+}
